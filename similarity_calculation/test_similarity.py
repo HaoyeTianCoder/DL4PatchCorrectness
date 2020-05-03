3cc24d887f5c9d335a81aa056a6ac87e566a604b
Abdoul Kader KABORE <derokabore@gmail.com>
2020-05-03 12:09:36 +0200

    Results for 'test_similarity_repair_tool'

--- similarity_calculation/test_similarity.py
+++ similarity_calculation/test_similarity.py
@@ -4,15 +4,16 @@ import pandas as pd
 import re
 from bert_serving.client import BertClient
 from sklearn.metrics.pairwise import cosine_similarity
-from gensim.models import word2vec,Doc2Vec
+from gensim.models import word2vec,Doc2Vec, KeyedVectors
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from nltk.tokenize import word_tokenize
 import json
+from utils import my_split
+
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', -1)
-
 
 path_patch_test = '/Users/haoye.tian/Documents/University/data/kui_patches/Patches_test'
 
@@ -20,6 +21,168 @@ root_new = '/Users/haoye.tian/Documents/University/data/experiment2'
 benchmarks = ["Bears", "Bugs.jar", "Defects4J", "IntroClassJava","QuixBugs"]
 # benchmarks = ['QuixBugs']
 tools = ["Arja", "GenProg", "Kali", "RSRepair", "Cardumen", "jGenProg", "jKali", "jMutRepair", "Nopol", "DynaMoth", "NPEFix"]
+
+# Begin code2vec
+import io
+import datetime
+import subprocess
+
+EXTRACTOR_JAR = '/Users/abdoulkader.kabore/snt/code2vec/JavaExtractor/JPredict/target/JavaExtractor-0.0.1-SNAPSHOT.jar'
+MAX_PATH_LENGTH = 8 
+MAX_PATH_WIDTH = 2
+TMP_FOLDER = 'tmp'
+
+code2vec_path = '/Users/abdoulkader.kabore/snt/code2vec/models/code2vec/w2v_tokens_format.txt'
+path_patch_test = '../data/code2vec_train_data/Patches_test'
+root_new = '../data/experiment2_code2vec'
+
+print('Loading code2vec pretrained model')
+code2vec_model = KeyedVectors.load_word2vec_format(code2vec_path, binary=False)
+
+try:
+    os.makedirs(TMP_FOLDER)
+except OSError:
+    pass
+
+def get_diff_files(file_path, type, withFile=True):
+    if withFile:
+        file = open(file_path, 'r')
+    else:
+        file = file_path
+
+    lines = ''
+    p = r"([^\w_|@|$|:])"
+    flag = True
+        
+    for line in file:
+        line = line.strip()
+        if '*/' in line:
+            flag = True
+            continue
+        if flag == False:
+            continue
+        if line != '':
+            if line.startswith('@@') or line.startswith('diff') or line.startswith('index') or line.startswith('---') or line.startswith('+++'):
+                continue
+            elif '/*' in line:
+                flag = False
+                continue
+            elif type == 'buggy':
+                if line.startswith('-'):
+                    if line[1:].strip().startswith('//'):
+                        continue
+                    lines += line[1:] + ' '
+                elif line.startswith('+'):
+                    # do nothing
+                    pass
+                else:
+                    lines += line + ' '
+            elif type == 'patched':
+                if line.startswith('+'):
+                    if line[1:].strip().startswith('//'):
+                        continue
+                    lines += line[1:] + ' '
+                elif line.startswith('-'):
+                    # do nothing
+                    pass
+                else:
+                    lines += line + ' '
+   
+    if withFile:
+        file.close()
+
+    return lines
+
+def code2vec(df, code2vec_path, threshold):
+
+    block = ''
+    length =df.shape[0]
+    bug_id = str(df['bugid'][0])
+    block += '**************\n'
+    block += 'Bugid: {}, patches: {} \n'.format(bug_id,length)
+
+    # tokenize
+    df['buggy'] = df['buggy'].map(lambda x: my_split(x))
+    df['patched'] = df['patched'].map(lambda x: my_split(x))
+    # result = cosine_similarity(bc.encode(list(np.array(df_quick['buggy']))),bc.encode(list(np.array(df_quick['patched']))))
+    df['simi'] = None
+    
+    for index,row in df.iterrows():
+        print('{}/{}'.format(index,length))
+        if row['buggy'] == [] or row['patched'] == []:
+            print('buggy or patched is []')
+            continue
+        try:
+            buggy_tokens = []
+            patch_tokens = []
+
+            for i, b in enumerate(row['buggy']):
+                if b in code2vec_model.vocab:
+                    buggy_tokens.append(b)
+
+            for i, b in enumerate(row['patched']):
+                if b in code2vec_model.vocab:
+                    patch_tokens.append(b)
+
+            bug_vec = code2vec_model[buggy_tokens]
+            patched_vec = code2vec_model[patch_tokens]
+        except Exception as e:
+            print(e)
+            continue
+        result = cosine_similarity(bug_vec,patched_vec)
+        df.loc[index,'simi'] = float(result[0][0])
+    df = df.sort_values(by='simi')
+    df.index = range(len(df))
+
+    return None, None, df
+
+def process_id_extraction(path, changes, type, id_path):
+    if not os.path.exists(id_path):
+        
+        file_path = path + '/' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S.%f") + '_PATCH.java'
+        writer = io.open(file_path, 'w', encoding='utf-8')
+        writer.write(changes)
+        writer.close()
+
+        command = 'java -cp ' + EXTRACTOR_JAR + ' JavaExtractor.App --max_path_length ' + \
+                    str(MAX_PATH_LENGTH) + ' --max_path_width ' + str(MAX_PATH_WIDTH) + ' --file ' + file_path + ' > ' + id_path
+        try:
+            subprocess.check_call(command, shell=True, stderr=subprocess.STDOUT)
+        except:
+            return None
+        
+        os.remove(file_path)
+
+    lines = []
+    with open(id_path, 'r') as ff:
+        for line in ff:
+            lines.append(line)
+    return lines
+
+def retrieve_identifiers(input):
+    data = []
+    if input is None:
+        return data
+    for line in input:
+        parts = line.rstrip('\n').split(' ')
+        target_name = parts[0]
+        contexts = parts[1:]
+        data.append(target_name)
+        for c in contexts:
+            tmp = c.split(',')
+            del tmp[1]
+            data.extend(tmp)
+
+    return unique_list(data)
+
+def unique_list(l):
+  x = []
+  for a in l:
+    if a not in x:
+      x.append(a)
+  return x
+
+# End
 
 def get_diff_files_frag(patch,type):
     # with open(patch, 'r') as file:
@@ -140,14 +303,37 @@ def test_similarity_repair_tool(path_patch_test, model, threshold):
                                         p_list = patch.split('\\n')
                                     else:
                                         print('error...')
-                                    buggy = get_diff_files_frag(p_list, type='buggy')
-                                    patched = get_diff_files_frag(p_list, type='patched')
-                                    sample = np.array(['1', bug_id, patch_id, buggy, patched, patch]).reshape((-1, 6))
-                                    if cnt == 0:
-                                        data = sample
+
+                                    if model == 'code2vec':
+                                        buggy = get_diff_files(p_list, type='buggy', withFile=False)
+                                        patched = get_diff_files(p_list, type='patched', withFile=False)
                                     else:
-                                        data = np.concatenate((data, sample), axis=0)
-                                    cnt += 1
+                                        buggy = get_diff_files_frag(p_list, type='buggy')
+                                        patched = get_diff_files_frag(p_list, type='patched')
+
+                                    buggy_id_path = TMP_FOLDER + '/' + bug_id + '_BUG.id'
+                                    patch_id_path = TMP_FOLDER + '/' + patch_id + '_PATCH.id'
+
+                                    b_output = process_id_extraction(TMP_FOLDER, buggy, 'buggy', buggy_id_path)
+                                    if b_output is not None:
+                                        p_output = process_id_extraction(TMP_FOLDER, patched, 'patched', patch_id_path)
+
+                                        buggy_tokens = retrieve_identifiers(b_output)
+                                        patch_tokens = retrieve_identifiers(p_output)
+                                        
+                                        if len(buggy_tokens) > 0 and len(patch_tokens) > 0:
+                                            buggy = ' '.join(buggy_tokens)
+                                            patched = ' '.join(patch_tokens)
+
+                                            sample = np.array(['1', bug_id, patch_id, buggy, patched, patch]).reshape((-1, 6))
+                                            if cnt == 0:
+                                                data = sample
+                                            else:
+                                                data = np.concatenate((data, sample), axis=0)
+                                            cnt += 1
+
+                                if isinstance(data, str):
+                                    continue
 
                                 calulate_similarity(path_result,data,model,threshold)
 
@@ -155,15 +341,18 @@ def test_similarity_repair_tool(path_patch_test, model, threshold):
 def calulate_similarity(path_result,data, model, threshold):
     data = data.reshape((-1, 6))
     df = pd.DataFrame(data, dtype=str, columns=['label', 'bugid', 'patchid', 'buggy', 'patched', 'patch'])
-    # tokenize
-    df['buggy'] = df['buggy'].map(lambda x: word_tokenize(x))
-    df['patched'] = df['patched'].map(lambda x: word_tokenize(x))
+    if model != 'code2vec':
+        # tokenize
+        df['buggy'] = df['buggy'].map(lambda x: word_tokenize(x))
+        df['patched'] = df['patched'].map(lambda x: word_tokenize(x))
     df['simi'] = None
 
     if model == 'bert':
         df_threshold, df_top, df = bert(df, threshold)
     elif model == 'doc':
         df_threshold, df_top, df = doc(df, threshold)
+    elif model == 'code2vec':
+        df_threshold, df_top, df = code2vec(df, code2vec_path, threshold)
     else:
         print('wrong model')
         raise ('wrong model')
@@ -234,6 +423,8 @@ def test_similarity(path_patch_test, model, threshold):
                     df_threshold, df_top = bert(df, threshold)
                 elif model == 'doc':
                     df_threshold, df_top= doc(df, threshold)
+                elif model == 'code2vec':
+                    df_threshold, df_top, df = code2vec(df, code2vec_path, threshold)
                 else:
                     print('wrong model')
 
@@ -317,7 +508,7 @@ def doc(df, threshold):
     model = Doc2Vec.load('../data/doc_frag.model')
 
     for index, row in df.iterrows():
-        bug_vec = model.infer_vector(row['buggy'],alpha=0.025,steps=300)
+        bug_vec = model.infer_vector(row['buggy'] ,alpha=0.025,steps=300)
         patched_vec = model.infer_vector(row['patched'],alpha=0.025,steps=300)
         # similarity calculation
         result = cosine_similarity(bug_vec.reshape((1,-1)), patched_vec.reshape((1,-1)))
@@ -353,4 +544,6 @@ if __name__ == '__main__':
     # threshold = [0.28489, 0.91795, 0.93891, 0.8580]
 
     # test_similarity(path_patch_test,model=model,threshold=threshold[1])
+
+    model = 'code2vec'
     test_similarity_repair_tool(path_patch_test,model=model,threshold=None)
